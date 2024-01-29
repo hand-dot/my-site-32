f1004dce1dffb10205387c60e6780afe553722c1
Kyohei Fukuda <kyouhei.fukuda0729@gmail.com>
2024-01-29 12:15:11 +0900

    TMP

--- tests/real.test.js
+++ tests/real.test.js
@@ -1,23 +1,36 @@
 import { test, expect, describe, vi } from "vitest";
 import { emailCurrentMember } from "../src/backend/real.js"
-
+import { triggeredEmails } from 'wix-crm-backend';
+import { mocks } from "../velo-vite-test-kit.js"
 
 describe('real.js', () => {
-  test('emailCurrentMember', async () => {
+  test('emailCurrentMember 1', async () => {
 
-    // const spy = vi.spyOn(triggeredEmails, "emailMember");
+    const spy = vi.spyOn(triggeredEmails, "emailMember");
 
     const emailId = "U2GDBS7";
     const res = await emailCurrentMember(emailId);
 
     expect(res).toBe(undefined);
-    // expect(spy).toHaveBeenCalledWith("U2GDBS7",
-    //   "d9cde4f8-cd5d-42d7-a827-a59d8a7789b8",
-    //   {
-    //     variables: {
-    //       firstName: "Kyohei",
-    //       lastName: "Fukuda"
-    //     }
-    //   });
+    expect(spy).toHaveBeenCalledWith("U2GDBS7",
+      "d9cde4f8-cd5d-42d7-a827-a59d8a7789b8",
+      {
+        variables: {
+          firstName: "Kyohei",
+          lastName: "Fukuda"
+        }
+      });
+  });
+  test('emailCurrentMember 2', async () => {
+    triggeredEmails.emailMember.mockImplementation(mocks["wix-crm-backend"].triggeredEmails.emailMember.fail)
+
+    const emailId = "U2GDBS7";
+
+    try {
+      const res = await emailCurrentMember(emailId);
+      expect(res).toBe(undefined);
+    } catch (e) {
+      expect(e).toBe("error");
+    }
   });
 });
